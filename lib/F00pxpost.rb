86e2ba2378121e5b164798fffbf39497b62569c0
Laurie Young <laurie@wildfalcon.com>
2013-05-27 00:16:22 +0100

    Basic outline for uploading a photo

--- lib/F00pxpost.rb
+++ lib/F00pxpost.rb
@@ -1,5 +1,16 @@
 require "f00pxpost/version"
+require "f00pxpost/client"
 
 module F00pxpost
-  
+
+  class << self
+
+    attr_accessor :consumer_key, :consumer_secret
+
+    def setup
+      yield(self)
+    end
+
+  end
+
 end
---
+++ lib/F00pxpost/client.rb
@@ -0,0 +1,41 @@
+module F00pxpost
+  class Client
+    
+    attr_accessor :token, :secret
+
+    def initialize(token, secret)
+      @token = token
+      @secret = secret
+      @client = OAuth::AccessToken.new(oauth_consumer, token,secret)               
+    end
+
+    def post_photo(file, title)
+      response = JSON.parse @client.post("/v1/photos", 
+                    {name: title, description: title, category: 1}).body
+      
+      upload_key = response["upload_key"]
+      photo_id = response["photo"]["id"]
+    
+      response = RestClient.post 'https://api.500px.com/v1/upload', 
+                                  file: file, 
+                                  photo_id: photo_id,
+                                  upload_key: upload_key,
+                                  consumer_key: F00pxpost.consumer_key,
+                                  access_key: @token
+
+      puts response.body
+                    
+      {key: upload_key, id: photo_id}
+    end
+        
+    def oauth_consumer
+      @oauth_consumer ||= begin 
+        key = F00pxpost.consumer_key
+        secret = F00pxpost.consumer_secret
+        opts = {:site => "https://api.500px.com"}
+        OAuth::Consumer.new(key, secret, opts)
+      end
+       
+    end
+  end
+end
